5d91fd8d9ab6d9577ada2bc12d45df2e1e559198
Izaque <izaque.l2004@aluno.ifsc.edu.br>
2022-02-24 17:02:50 -0300

    Inicio Do Projeto

---
+++ src/aula01/GitHub.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author IFSC
+ */
+public class GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner leia = new Scanner(System.in);
+       
+       String nome;
+       
+       System.out.println("Digite seu nome:");
+       nome = leia.nextLine();
+       
+        System.out.println("Nome: "+nome);
+        
+        
+        
+    }
+    
+}
